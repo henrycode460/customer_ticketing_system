8f730a38ad659664a07a74e3b38227d48d039d0a
Henry <dokinruss@gmail.com>
2023-08-23 09:20:30 +0000

    updatee guniorn-cfp

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,8 +1,8 @@
 
+import multiprocessing
 
-bind = '0.0.0.0:5005'
-workers = 1
-accesslog = '-'
-loglevel = 'debug'
-capture_output = True
-enable_stdio_inheritance = True
+bind = "0.0.0.0:8000"  # Gunicorn will bind to all available network interfaces
+workers = multiprocessing.cpu_count() * 2 + 1  # Adjust the number of workers as needed
+accesslog = "-"  # You can customize the log paths
+errorlog = "-"
+
